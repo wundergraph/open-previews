e1bf14a4f7fe6c59d9e52c1c67db9ca3c3dc460b
Pagebakers <eelco@appulse.nl>
2023-05-02 17:42:04 +0200

    chore: revert to serverless

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,7 +2,7 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
