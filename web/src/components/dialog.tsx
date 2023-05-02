e670e0f1e61e03f03d19821dcb7487c12829f24d
Pagebakers <eelco@appulse.nl>
2023-05-02 17:22:07 +0200

    chore: test

--- web/src/components/dialog.tsx
+++ web/src/components/dialog.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import clsx from "clsx";
-import { X } from "lucide-react";
+// import { X } from "lucide-react";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -51,7 +51,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-        <X className="h-4 w-4" />
+        {/* <X className="h-4 w-4" /> */}
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
