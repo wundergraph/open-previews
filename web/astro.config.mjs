fd85a3233ad36b2480362fb520a5d9823b74c71b
Pagebakers <eelco@appulse.nl>
2023-05-03 16:02:20 +0200

    chore: fix build

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,12 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://openpreviews.com/",
   output: "server",
-  adapter: vercel(),
+  adapter: import.meta.env.VERCEL
+    ? vercel()
+    : node({
+        mode: "standalone",
+      }),
   integrations: [
     tailwind(),
     react(),
