24dfd6813bd37e23db211b00a81ca96bb05edd71
DaniAkash <s.daniakash@gmail.com>
2023-06-28 11:31:48 +0530

    chore: generated styles

--- app/styled-system/types/prop-type.d.ts
+++ app/styled-system/types/prop-type.d.ts
@@ -197,7 +197,7 @@ type PropertyValueTypes  = {
 	stroke: Tokens["colors"];
 	srOnly: boolean;
 	debug: boolean;
-	colorPalette: "rose" | "pink" | "fuchsia" | "purple" | "indigo" | "blue" | "sky" | "cyan" | "teal" | "green" | "lime" | "yellow" | "orange" | "red" | "gray" | "slate" | "fg" | "fg.inverted" | "bg" | "accent" | "border";
+	colorPalette: "rose" | "pink" | "fuchsia" | "purple" | "violet" | "indigo" | "blue" | "sky" | "cyan" | "teal" | "emerald" | "green" | "lime" | "yellow" | "amber" | "orange" | "red" | "neutral" | "stone" | "zinc" | "gray" | "slate" | "fg" | "fg.inverted" | "bg" | "accent" | "border";
 	textStyle: "8xl" | "9xl" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "6xl" | "7xl";
 }
 
